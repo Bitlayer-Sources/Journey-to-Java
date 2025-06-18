f39cbecb5a26cb31e66d8045924a4f3058673759
bitlayer-source <gaele1440@outlook.com>
2025-06-17 20:32:21 -0400

    Add files via upload

---
+++ Arrayable.java
@@ -0,0 +1,16 @@
+
+public class Arrayable {
+
+	public static void main(String[] args) {
+		
+		String[] cars = new String[3];
+		
+		cars[0] = "Corvette";
+		cars[1] = "Bugatti";
+		cars[2] = "Camaro";
+		
+		System.out.println(cars[0]);
+
+	}
+
+}
