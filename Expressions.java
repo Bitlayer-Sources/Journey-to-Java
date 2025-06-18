ac04326ab73a2fb9cd41100429b55360f1b969be
bitlayer-source <gaele1440@outlook.com>
2025-06-17 20:33:22 -0400

    Add files via upload

---
+++ Expressions.java
@@ -0,0 +1,16 @@
+
+public class Expressions {
+
+	public static void main(String[] args) {
+		
+		// There is a party going on right now
+		
+		double friends = 62;
+		
+		friends = (double) friends / 3;
+		
+		System.out.println(friends);
+
+	}
+
+}
---
+++ ForLoopinger.java
@@ -0,0 +1,18 @@
+
+public class ForLoopinger {
+
+	public static void main(String[] args) {
+		
+		// Hide and seek countdown with Java
+		
+		// For Loop for countdown
+		for (int i = 5; i >= 0; i--) {
+			System.out.println(i);
+		}
+		
+		// Seeker is ready
+		System.out.println("Suprise!");
+
+	}
+
+}
